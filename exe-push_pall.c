6e17ab62beb2df6fad332c3b1917bd93a69fdac4
Tariroyashe <rukodzac@gmail.com>
2023-10-20 01:59:28 -0700

    Implement the push and pall opcodes

--- exe-push_pall.c
+++ exe-push_pall.c
@@ -120,20 +120,60 @@ return (-1);
 return (pos);
 }
 
+
 /**
-* free_stack - Frees memory associated with the stack.
-* @stack: A pointer to the top of the stack
+* main - Monty ByteCodes interpreter main function
+* @argc: The number of command-line arguments
+* @argv: An array of command-line arguments
 *
 * Description:
-* - This function iterates through the stack and frees the memory
-*   allocated for each node, ensuring there are no memory leaks.
+* - Checks command-line arguments and opens the Monty ByteCode file.
+* - Reads and interprets the file line by line, executing valid opcodes.
+*
+* Return: 0 on success, EXIT_FAILURE on error.
 */
-void free_stack(stack_t *stack)
+int main(int argc, char *argv[])
 {
-while (stack)
+char *opcode;
+stack_t *stack;
+char *line = NULL;
+size_t len = 0;
+unsigned int line_number = 0;
+FILE *file;
+if (argc != 2)
 {
-stack_t *temp = stack;  /* Create a temporary pointer to the current node */
-stack = stack->next;    /* Move to the next node */
-free(temp);             /* Free the memory of the current node */
+fprintf(stderr, "USAGE: monty file\n");
+exit(EXIT_FAILURE);
 }
+
+file = fopen(argv[1], "r");
+if (file == NULL)
+{
+fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+exit(EXIT_FAILURE);
+}
+while (custom_getline(&line, &len, file) != -1)
+{
+line_number++;
+opcode = strtok(line, " \t\n");
+if (opcode == NULL || opcode[0] == '#')
+continue;  /*Skip empty lines and comments*/
+if (strcmp(opcode, "push") == 0)
+{
+push(&stack, line_number);
+}
+else if (strcmp(opcode, "pall") == 0)
+{
+pall(&stack);
+}
+/* Add more opcode cases here as needed*/
+else
+{
+fprintf(stderr, "L%u: unknown instruction %s\n", line_number, opcode);
+exit(EXIT_FAILURE);
+}
+}
+fclose(file);
+free(line);
+return (0);
 }
